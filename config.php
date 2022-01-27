8f75610217f1a454a672467033c4c5f563ef0604
starfruit1437 <98253570+starfruit1437@users.noreply.github.com>
2022-01-27 21:41:04 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // 网站标题
-const title = "荆棘留言板";
+const title = "南宁十四中19级留言板";
 // 数据库地址
 const db_host = "233.233.233.233";
 // 数据库名
@@ -8,6 +8,6 @@ const db_name = "name";
 // 数据库用户名
 const db_username = "username";
 // 数据库密码
-const db_password = "password";
+const db_password = "820325";
 // 数据库端口
 const db_port = 3306;
